805c3339ad5020594f46961b9341695b39a4e778
capitalistspz <keipitalists@protonmail.com>
2021-12-17 16:27:15 +0000

    Updated to Minecraft 1.18.1 and Java 17

--- src/main/java/capitalistspz/test/mixin/MinecraftServerMxn.java
+++ src/main/java/capitalistspz/test/mixin/MinecraftServerMxn.java
@@ -3,6 +3,7 @@ package capitalistspz.test.mixin;
 import capitalistspz.test.SnowballKB;
 import capitalistspz.test.config.Config;
 import net.minecraft.server.MinecraftServer;
+import org.apache.logging.log4j.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,7 +12,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerMxn {
     @Inject(method="save",at=@At("TAIL"))
-    public void saveConfig(boolean suppressLogs, boolean bl, boolean bl2, CallbackInfoReturnable<Boolean> cir){
-        Config.save(SnowballKB.config);
+    public void saveConfig(boolean suppressLogs, boolean flush, boolean force, CallbackInfoReturnable<Boolean> cir){
+        if (!suppressLogs && Config.save(SnowballKB.config)){
+            SnowballKB.logger.log(Level.INFO, "Config saved.");
+        }
     }
 }
