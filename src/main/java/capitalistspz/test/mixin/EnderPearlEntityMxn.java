917e286fe3a1bc7231212619478edb597812e690
MightyKnight <38622942+MightyKnight@users.noreply.github.com>
2025-05-02 23:14:28 +0200

    Add enderpearl knockback/damage and use `WrapOperation` for mixins

--- src/main/java/capitalistspz/test/mixin/EnderPearlEntityMxn.java
+++ src/main/java/capitalistspz/test/mixin/EnderPearlEntityMxn.java
@@ -7,17 +7,17 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.thrown.EggEntity;
+import net.minecraft.entity.projectile.thrown.EnderPearlEntity;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+@Mixin(EnderPearlEntity.class)
+public abstract class EnderPearlEntityMxn extends ThrownItemEntity {
 
-@Mixin(EggEntity.class)
-public abstract class EggEntityMxn extends ThrownItemEntity {
-    public EggEntityMxn(EntityType<? extends ThrownItemEntity> entityType, World world) {
+    public EnderPearlEntityMxn(EntityType<? extends ThrownItemEntity> entityType, World world) {
         super(entityType, world);
     }
 
@@ -27,17 +27,18 @@ public abstract class EggEntityMxn extends ThrownItemEntity {
     protected void onHitPlayer(Entity entity, DamageSource source, float amount, Operation<Void> original) {
         if (entity instanceof PlayerEntity player && !player.getAbilities().invulnerable)
         {
-            if (SnowballKB.config.eggTraditionalKb){
+            if (SnowballKB.config.pearlTraditionalKb){
                 var yaw = -this.getBodyYaw();
-                player.takeKnockback(SnowballKB.config.eggKbMultiplier, MathHelper.sin(yaw * SnowballKB.DegToRad), -MathHelper.cos(yaw * SnowballKB.DegToRad));
+                player.takeKnockback(SnowballKB.config.pearlKbMultiplier, MathHelper.sin(yaw * SnowballKB.DegToRad), -MathHelper.cos(yaw * SnowballKB.DegToRad));
             }
             else {
-                player.setVelocity(player.getVelocity().add(this.getVelocity().normalize().multiply(SnowballKB.config.eggKbMultiplier)));
-                player.velocityModified = true;
+                entity.setVelocity(entity.getVelocity().add(this.getVelocity().normalize().multiply(SnowballKB.config.pearlKbMultiplier)));
+                entity.velocityModified = true;
             }
 
-            original.call(entity, source, SnowballKB.config.eggDamage);
+            original.call(entity, source, SnowballKB.config.pearlDamage);
         }
 
     }
+
 }
